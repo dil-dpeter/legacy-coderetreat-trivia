15d0b83c1e3cb435c35d341911c8b47d8c507918
Péter Dávid <dpeter@diligent.com>
2024-12-02 09:53:27 +0000

    test2

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
+#include <gtest/gtest.h>
 #include "..\game.h"
+#include "CppUnitTest.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
@@ -8,6 +11,12 @@ TEST(TestCaseName, TestName) {
 class GameTest : public ::testing::Test {
 protected:
     Game game;  // Create an instance of Game
+    void setNumberOfPlayers(int numPlayers) {
+
+        for (int i = 0; i < numPlayers; ++i) {
+            game.add("Player" + std::to_string(i + 1));
+        }
+    }
 };
 
 TEST_F(GameTest, CreateRockQuestion) {
@@ -15,4 +24,17 @@ TEST_F(GameTest, CreateRockQuestion) {
     std::string expected = "Rock Question 5";
     std::string result = game.createRockQuestion(index);
     EXPECT_EQ(result, expected);
+}
+
+TEST_F(GameTest, IsPlayable) {
+   
+    setNumberOfPlayers(1);
+    EXPECT_FALSE(game.isPlayable());
+
+
+    setNumberOfPlayers(2);
+    EXPECT_TRUE(game.isPlayable());
+
+    setNumberOfPlayers(3);
+    EXPECT_TRUE(game.isPlayable());
 }
